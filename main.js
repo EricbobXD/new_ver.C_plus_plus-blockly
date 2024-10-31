6335a47a894249336ec58b2edfd6d79d222a9969
Coderex._.bob <140780835+EricbobXD@users.noreply.github.com>
2024-10-31 09:15:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,40 +2,117 @@ const toolbox = {
     "kind": "categoryToolbox",
     "contents": [
         {
-            "kind": "category",
-            "name": "Logic",
-            "contents": [
+          "categories": [
+            {
+              "name": "vector",
+              "colour": 230,
+              "blocks": [ "create_vector", "push_back", "pop_back", "clear", "size" ]
+            },
+            {
+              "name": "主程式框架",
+              "colour": 0,
+              "blocks": [
                 {
-                    "kind": "category",
-                    "name": "Logic",
-                    "contents": [
-                        { "kind": "block", "type": "controls_if" },
-                        { "kind": "block", "type": "logic_compare" },
-                        { "kind": "block", "type": "logic_operation" }
-                    ]
+                  "type": "include_bits/stdc++.h",
+                  "next": {
+                    "type": "using_namespace_std",
+                    "next": {
+                      "type": "main_block"
+                    }
+                  }
+                }
+              ]
+            },
+            {
+              "name": "自定義函式",
+              "colour": 290,
+              "blocks": [ "basic_function_definition", "function_call" ]
+            },
+            {
+              "name": "自定義變數",
+              "colour": 60,
+              "blocks": [ "variable_declaration", "variable_assignment", "variable_get" ]
+            },
+            {
+              "name": "輸入與輸出",
+              "colour": 120,
+              "blocks": [ "cin_block", "cout_block", "scanf_block", "printf_block", "boost_ios_sync", "boost_cin_tie", "boost_cout_tie" ]
+            },
+            {
+              "name": "邏輯運算子",
+              "colour": 210,
+              "blocks": [ "logic_operators", "or_and_xor", "logic_not" ]
+            },
+            {
+              "name": "數學與運算",
+              "colour": 230,
+              "blocks": [ "number", "math_caculacte", "math_sqrt", "math_abs", "math_sine", "math_cosine", "math_tangent", "math_ceil", "math_floor", "math_random" ]
+            },
+            {
+              "name": "條件與迴圈",
+              "colour": 260,
+              "blocks": [
+                "controls_if",
+                {
+                  "type": "switch_block",
+                  "statement": {
+                    "DO": {
+                      "blocks": [
+                        {
+                          "type": "case_block",
+                          "statement": {
+                            "DO": {
+                              "blocks": [
+                                "break_block"
+                              ]
+                            }
+                          },
+                          "next": {
+                            "type": "default_block",
+                            "statement": {
+                              "DO": {
+                                "blocks": [
+                                  "break_block"
+                                ]
+                              }
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  }
                 },
                 {
-                    "kind": "category",
-                    "name": "Loops",
-                    "contents": [
-                        { "kind": "block", "type": "controls_repeat_ext" },
-                        { "kind": "block", "type": "controls_whileUntil" }
-                    ]
-                }
-            ]
-        },
-        {
-            "kind": "category",
-            "name": "Loops",
-            "contents": [{
-                    "kind": "block",
-                    "type": "controls_repeat_ext"
+                  "type": "case_block",
+                  "statement": {
+                    "DO": {
+                      "blocks": [
+                        "break_block"
+                      ]
+                    }
+                  }
                 },
-                {
-                    "kind": "block",
-                    "type": "controls_whileUntil"
-                }
-            ]
+                "for_block",
+                "while_block",
+                "break_block",
+                "continue_block",
+                "return_block"
+              ]
+            },
+            {
+              "name": "文本操作",
+              "colour": 160,
+              "blocks": [
+                "text",
+                "comment_block"
+              ]
+            },
+            {
+              "name": "測試 Block",
+              "colour": 290,
+              "blocks": []
+            }
+          ]
         }
     ]
 };

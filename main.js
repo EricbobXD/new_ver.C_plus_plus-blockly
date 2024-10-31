fa7bdff95b966e182fdc9de084a634824c4e1d30
Coderex._.bob <140780835+EricbobXD@users.noreply.github.com>
2024-10-31 10:54:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,120 +1,108 @@
 const toolbox = {
-/*    "kind": "categoryToolbox",
-    "contents": [
-        {
-          "categories": [
-            {
-              "name": "vector",
-              "colour": 230,
-              "blocks": [ "create_vector", "push_back", "pop_back", "clear", "size" ]
-            },
-            {
-              "name": "主程式框架",
-              "colour": 0,
-              "blocks": [
-                {
-                  "type": "include_bits/stdc++.h",
-                  "next": {
-                    "type": "using_namespace_std",
-                    "next": {
-                      "type": "main_block"
-                    }
-                  }
-                }
-              ]
-            },
-            {
-              "name": "自定義函式",
-              "colour": 290,
-              "blocks": [ "basic_function_definition", "function_call" ]
-            },
-            {
-              "name": "自定義變數",
-              "colour": 60,
-              "blocks": [ "variable_declaration", "variable_assignment", "variable_get" ]
-            },
-            {
-              "name": "輸入與輸出",
-              "colour": 120,
-              "blocks": [ "cin_block", "cout_block", "scanf_block", "printf_block", "boost_ios_sync", "boost_cin_tie", "boost_cout_tie" ]
-            },
-            {
-              "name": "邏輯運算子",
-              "colour": 210,
-              "blocks": [ "logic_operators", "or_and_xor", "logic_not" ]
-            },
-            {
-              "name": "數學與運算",
-              "colour": 230,
-              "blocks": [ "number", "math_caculacte", "math_sqrt", "math_abs", "math_sine", "math_cosine", "math_tangent", "math_ceil", "math_floor", "math_random" ]
-            },
-            {
-              "name": "條件與迴圈",
-              "colour": 260,
-              "blocks": [
-                "controls_if",
-                {
-                  "type": "switch_block",
-                  "statement": {
-                    "DO": {
-                      "blocks": [
-                        {
-                          "type": "case_block",
-                          "statement": {
-                            "DO": {
-                              "blocks": [
-                                "break_block"
-                              ]
-                            }
-                          },
-                          "next": {
-                            "type": "default_block",
-                            "statement": {
-                              "DO": {
-                                "blocks": [
-                                  "break_block"
-                                ]
-                              }
-                            }
-                          }
-                        }
-                      ]
-                    }
-                  }
-                },
-                {
-                  "type": "case_block",
-                  "statement": {
-                    "DO": {
-                      "blocks": [
-                        "break_block"
-                      ]
-                    }
-                  }
-                },
-                "for_block",
-                "while_block",
-                "break_block",
-                "continue_block",
-                "return_block"
-              ]
-            },
-            {
-              "name": "文本操作",
-              "colour": 160,
-              "blocks": [
-                "text",
-                "comment_block"
-              ]
-            },
-            {
-              "name": "測試 Block",
-              "colour": 290,
-              "blocks": []
-            }
-          ]
-        }
-    ]*/
+    `
+        <xml>
+                <category name="vector" colour="230">
+                    <block type="create_vector"></block>
+                    <block type="push_back"></block>
+                    <block type="pop_back"></block>
+                    <block type="clear"></block>
+                    <block type="size"></block>
+                </category>
+                
+                <category name="主程式框架" colour="0">
+                    <block type="include_bits/stdc++.h">
+                        <next>
+                          <block type="using_namespace_std">
+                            <next>
+                              <block type="main_block"></block>
+                            </next>
+                          </block>
+                        </next>
+                    </block>
+                </category>
+                
+                <category name="自定義函式" colour="290">
+                    <block type="basic_function_definition"></block>
+                    <block type="function_call"></block>
+                </category>
+                <sep></sep>
+
+                <category name="自定義變數" colour="60">
+                    <block type="variable_declaration"></block>
+                    <block type="variable_assignment"></block>
+                    <block type="variable_get"></block>
+                </category>
+
+                <category name="輸入與輸出" colour="120">
+                    <block type="cin_block"></block>
+                    <block type="cout_block"></block>
+                    <block type="scanf_block"></block>
+                    <block type="printf_block"></block>
+                    <block type="boost_ios_sync"></block>
+                    <block type="boost_cin_tie"></block>
+                    <block type="boost_cout_tie"></block>
+                </category>
+
+                <category name="邏輯運算子" colour="210">
+                    <block type="logic_operators"></block>
+                    <block type="or_and_xor"></block>
+                    <block type="logic_not"></block>
+                </category>
+
+                <category name="數學與運算" colour="230">
+                    <block type="number"></block>
+                    <block type="math_caculacte"></block>
+                    <block type="math_sqrt"></block>
+                    <block type="math_abs"></block>
+                    <block type="math_sine"></block>
+                    <block type="math_cosine"></block>
+                    <block type="math_tangent"></block>
+                    <block type="math_ceil"></block>
+                    <block type="math_floor"></block>
+                    <block type="math_random"></block>
+                </category>
+
+                <category name="條件與迴圈" colour="260">
+                    <block type="controls_if"></block>
+                    <block type="switch_block">
+                        <statement name="DO">
+                          <block type="case_block">
+                            <statement name="DO">
+                              <block type="break_block"></block>
+                            </statement>
+                            <next>
+                              <block type="default_block">
+                                <statement name="DO">
+                                  <block type="break_block"></block>
+                                </statement>
+                                  </block>
+                                </next>
+                           </block>
+                        </statement>
+                    </block>
+                    <block type="case_block">
+                        <statement name="DO">
+                            <block type="break_block"></block>
+                        </statement>
+                    </block>
+                    <block type="for_block"></block>
+                    <block type="while_block"></block>
+                    <block type="break_block"></block>
+                    <block type="continue_block"></block>
+                    <block type="return_block"></block>
+                </category>
+
+                <category name="文本操作" colour="160">
+                    <block type="text"></block>
+                    <block type="comment_block"></block>
+                </category>
+                
+                <sep></sep>
+                
+                <category name="測試 Block" colour="290">
+                </category>
+        </xml>
+    `
 };
 
 var workspace = Blockly.inject('blockly-workspace', {
